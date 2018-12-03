e9edca86e838c57bbefd7d330c3969fb7e7bb8fd
zjuday <judayzj@rose-hulman.edu>
2018-12-03 11:40:16 -0500

    DONE

--- src/m3_graphical_accumulating.py
+++ src/m3_graphical_accumulating.py
@@ -9,8 +9,8 @@ Additionally, it emphasizes that you must
 before you can implement a solution to the problem in Python.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher, Mark Hays,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Aaron Wilkin, their colleagues, and Zachary Juday.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
